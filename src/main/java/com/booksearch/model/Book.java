148f6c4bd0fa0f4b8365f3114e06f3820ded4a4b
snailxxd <2559572532@qq.com>
2025-12-07 14:31:59 +0800

    feat: 为 Book 类补充属性

--- src/main/java/com/booksearch/model/Book.java
+++ src/main/java/com/booksearch/model/Book.java
@@ -4,14 +4,35 @@ import lombok.*;
 import java.time.LocalDate;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class Book {
     private String isbn;
     private String title;
     private String author;
+    private String language;
     private String publisher;
     private LocalDate publicationDate;
-    private double price;
-    private String description;
+    private double salePrice;
+    private double originalPrice;
+    private String category;
+    private String contents;
+    private String bookDescription;
+    private String authorDescription;
+    private String editorRecommendation;
+
+    public Book() {
+        isbn = "<UNK>";
+        title = "<UNK>";
+        author = "<UNK>";
+        language = "<UNK>";
+        publisher = "<UNK>";
+        publicationDate = LocalDate.now();
+        salePrice = 0.0;
+        originalPrice = 0.0;
+        category = "<UNK>";
+        contents = "<UNK>";
+        bookDescription = "<UNK>";
+        authorDescription = "<UNK>";
+        editorRecommendation = "<UNK>";
+    }
 }
